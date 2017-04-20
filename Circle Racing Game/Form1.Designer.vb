87ac2fea71c29e059bed0a9e4f75eb404020a135
Rk <r.kprajapati215@gmail.com>
2017-04-20 10:58:55 +0530

    Add files via upload

---
+++ Circle Racing Game/Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.OvalShape3 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.OvalShape2 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.OvalShape1 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.LineShape3 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape2 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.TimerRed = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerBlue = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerGreen = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.lb = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.OvalShape3, Me.OvalShape2, Me.OvalShape1, Me.LineShape3, Me.LineShape2, Me.LineShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(876, 507)
+        Me.ShapeContainer1.TabIndex = 0
+        Me.ShapeContainer1.TabStop = False
+        '
+        'OvalShape3
+        '
+        Me.OvalShape3.BackColor = System.Drawing.Color.Green
+        Me.OvalShape3.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.OvalShape3.Location = New System.Drawing.Point(29, 252)
+        Me.OvalShape3.Name = "OvalShape3"
+        Me.OvalShape3.Size = New System.Drawing.Size(70, 72)
+        '
+        'OvalShape2
+        '
+        Me.OvalShape2.BackColor = System.Drawing.Color.Blue
+        Me.OvalShape2.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.OvalShape2.FillColor = System.Drawing.Color.MediumBlue
+        Me.OvalShape2.Location = New System.Drawing.Point(29, 158)
+        Me.OvalShape2.Name = "OvalShape2"
+        Me.OvalShape2.Size = New System.Drawing.Size(70, 72)
+        '
+        'OvalShape1
+        '
+        Me.OvalShape1.BackColor = System.Drawing.Color.Red
+        Me.OvalShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+        Me.OvalShape1.FillColor = System.Drawing.Color.Red
+        Me.OvalShape1.FillGradientColor = System.Drawing.Color.White
+        Me.OvalShape1.FillGradientStyle = Microsoft.VisualBasic.PowerPacks.FillGradientStyle.Central
+        Me.OvalShape1.Location = New System.Drawing.Point(29, 67)
+        Me.OvalShape1.Name = "OvalShape1"
+        Me.OvalShape1.Size = New System.Drawing.Size(70, 72)
+        '
+        'LineShape3
+        '
+        Me.LineShape3.BorderColor = System.Drawing.SystemColors.AppWorkspace
+        Me.LineShape3.Name = "LineShape3"
+        Me.LineShape3.X1 = 67
+        Me.LineShape3.X2 = 740
+        Me.LineShape3.Y1 = 324
+        Me.LineShape3.Y2 = 324
+        '
+        'LineShape2
+        '
+        Me.LineShape2.BorderColor = System.Drawing.SystemColors.AppWorkspace
+        Me.LineShape2.Name = "LineShape2"
+        Me.LineShape2.X1 = 67
+        Me.LineShape2.X2 = 740
+        Me.LineShape2.Y1 = 229
+        Me.LineShape2.Y2 = 229
+        '
+        'LineShape1
+        '
+        Me.LineShape1.BorderColor = System.Drawing.SystemColors.AppWorkspace
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 67
+        Me.LineShape1.X2 = 740
+        Me.LineShape1.Y1 = 139
+        Me.LineShape1.Y2 = 138
+        '
+        'TimerRed
+        '
+        '
+        'TimerBlue
+        '
+        '
+        'TimerGreen
+        '
+        '
+        'Button1
+        '
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(3, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.DimGray
+        Me.Button1.Location = New System.Drawing.Point(33, 385)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 34)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Play"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.Control
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(3, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.DimGray
+        Me.Button2.Location = New System.Drawing.Point(661, 385)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(83, 34)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'lb
+        '
+        Me.lb.AutoSize = True
+        Me.lb.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lb.Location = New System.Drawing.Point(314, 385)
+        Me.lb.Name = "lb"
+        Me.lb.Size = New System.Drawing.Size(73, 23)
+        Me.lb.TabIndex = 3
+        Me.lb.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(876, 507)
+        Me.Controls.Add(Me.lb)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ShapeContainer1 As PowerPacks.ShapeContainer
+    Friend WithEvents OvalShape3 As PowerPacks.OvalShape
+    Friend WithEvents OvalShape2 As PowerPacks.OvalShape
+    Friend WithEvents OvalShape1 As PowerPacks.OvalShape
+    Friend WithEvents LineShape3 As PowerPacks.LineShape
+    Friend WithEvents LineShape2 As PowerPacks.LineShape
+    Friend WithEvents LineShape1 As PowerPacks.LineShape
+    Friend WithEvents TimerRed As Timer
+    Friend WithEvents TimerBlue As Timer
+    Friend WithEvents TimerGreen As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents lb As Label
+End Class
---
+++ Circle Racing Game/Form1.vb
@@ -0,0 +1,175 @@
+﻿Public Class Form1
+    'declare 7 globle integer variale which will be use in timer control.
+    Dim i, a, a1, b, b1, c1, c As Integer
+    Dim time As Integer = 0
+    Dim totaltime As Double
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        'CODING OF RESET BUTTON.
+
+        OvalShape1.Location = New Point(29, 67)
+        OvalShape2.Location = New Point(29, 158)
+        OvalShape3.Location = New Point(29, 252)
+        lb.Text = ""
+
+    End Sub
+    'FOR APPLYING RANDOM NUMBER FOR DECIDING FAIR   WINNER 
+    Dim r As New Random
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Button2.Enabled = False
+        'Enable all timer .
+        TimerBlue.Start()
+        TimerRed.Start()
+        TimerGreen.Start()
+
+    End Sub
+
+    'FOR RED CIRCLE CODE.
+
+    Private Sub TimerRed_Tick(sender As Object, e As EventArgs) Handles TimerRed.Tick
+
+        'GET THE VALUE OF OVAL OR CIRCLE  AND STOER THEM TO GLOBLE VARIABLE "a" AND "a1"
+
+        a = OvalShape1.Location.X
+        a1 = OvalShape1.Location.Y
+
+        'START OF LINESHAPE WIDTH IS "x1" AND END POINT IS "x2".
+        'SIMILIAR TO START OF LINESHAPE HEIGHT IS "Y1" AND END POINT IS "Y2".
+
+
+
+
+        'CHECK THE LINESHAPE(HORIZONTAL LINE) OF WIDTH  END POINT (X2) IS LESST THAN TO GLOBLE VARIABLE "a" OR NOT .
+
+        If a <= LineShape1.X2 Then
+
+            'HERE "r" IS RANDOM INTEGER NUMBER. WE DEFINE RANGE BETWEEN 1 AND 15.
+            'SYNTAX TO MOVE CIRCLE FROM OLD POSITION TO NEW  POSITION
+
+            ' CONTROLNAME.Location=
+            'New Point(X-axix(IN INTEGER),Y-axis(IN INTEGER))
+
+
+            OvalShape1.Location = New Point(a + (r.Next(1, 10)), a1)
+            If OvalShape1.FillColor = Color.Red Then
+                OvalShape1.FillColor = Color.DarkRed
+                OvalShape1.BorderColor = Color.White
+            ElseIf OvalShape1.FillColor = Color.DarkRed
+                OvalShape1.FillColor = Color.DarkOrange
+            Else
+                OvalShape1.FillColor = Color.Red
+                OvalShape1.BorderColor = Color.Red
+            End If
+
+            'FOR CALCULATING HOW MANY TIMES IT TAKES FOR FINISHING THE RACE.
+            time += 1  'INCREMENT THE VALUE OF GLOBLE VARIABLE time
+
+            'HERE TIMER INTERVAL IS 100ms BUT WE WANT 1s=1000. SO WE DIVIDE THIS VALUE TO BY 10 .MEANS 100*10=1000ms THAT IS 1s.
+            lb.Text = "Total Time is:" + CStr((Math.Round(time / 10)))
+
+        Else
+
+            'IT MEANS CIRCLE REACH END POINT SO STOP  ALL TIMER AND DECLARE A WINNER.
+
+
+            TimerRed.Enabled = False
+            TimerBlue.Enabled = False
+            Button2.Enabled = True
+            TimerGreen.Enabled = False
+            MsgBox("red is winner")
+
+        End If
+
+    End Sub
+
+    'FOR BLUE CIRCLE CODE.
+
+
+    Private Sub TimerBlue_Tick(sender As Object, e As EventArgs) Handles TimerBlue.Tick
+        'GET THE VALUE OF OVAL OR CIRCLE  AND STOER THEM TO GLOBLE VARIABLE "b" AND "b1"
+
+        b = OvalShape2.Location.X
+        b1 = OvalShape2.Location.Y
+
+        If b < LineShape2.X2 Then
+
+            'START OF LINESHAPE WIDTH IS "x1" AND END POINT IS "x2".
+            'SIMILIAR TO START OF LINESHAPE HEIGHT IS "Y1" AND END POINT IS "Y2".
+
+            ' CONTROLNAME.Location=
+            'New Point(X-axix(IN INTEGER),Y-axis(IN INTEGER))
+
+            OvalShape2.Location = New Point(b + (r.Next(1, 10)), b1)
+            If OvalShape2.FillColor = Color.Blue Then
+                OvalShape2.FillColor = Color.MediumBlue
+                OvalShape2.BorderColor = Color.White
+            ElseIf OvalShape2.FillColor = Color.MediumBlue
+                OvalShape2.FillColor = Color.LightBlue
+            ElseIf OvalShape2.FillColor = Color.LightBlue
+                OvalShape2.FillColor = Color.DarkCyan
+            Else
+                OvalShape2.FillColor = Color.Blue
+
+            End If
+        Else
+
+            'IT MEANS CIRCLE REACH END POINT SO STOP  ALL TIMER AND DECLARE A WINNER.
+            TimerBlue.Enabled = False
+            TimerRed.Enabled = False
+            TimerGreen.Enabled = False
+            Button2.Enabled = True
+            MsgBox("Blue is Winner")
+        End If
+
+
+
+    End Sub
+
+    'CODE FOR GREEN CIRCLE.
+    Private Sub TimerGreen_Tick(sender As Object, e As EventArgs) Handles TimerGreen.Tick
+        'GET THE VALUE OF OVAL OR CIRCLE  AND STOER THEM TO GLOBLE VARIABLE "c" AND "c1"
+
+
+        c = OvalShape3.Location.X
+        c1 = OvalShape3.Location.Y
+
+        If c <= LineShape3.X2 Then
+            'START OF LINESHAPE WIDTH IS "x1" AND END POINT IS "x2".
+            'SIMILIAR TO START OF LINESHAPE HEIGHT IS "Y1" AND END POINT IS "Y2".
+
+            ' CONTROLNAME.Location=
+            'New Point(X-axix(IN INTEGER),Y-axis(IN INTEGER))
+            OvalShape3.Location = New Point(c + (r.Next(1, 10)), c1)
+            If OvalShape3.FillColor = Color.Green Then
+                OvalShape3.FillColor = Color.DarkGreen
+                OvalShape3.BorderColor = Color.White
+            ElseIf OvalShape3.FillColor = Color.DarkGreen
+                OvalShape3.FillColor = Color.LightGreen
+
+            ElseIf OvalShape3.FillColor = Color.LightGreen
+                OvalShape3.FillColor = Color.LawnGreen
+            Else
+                OvalShape3.FillColor = Color.Green
+            End If
+
+        Else
+            'IT MEANS CIRCLE REACH END POINT SO STOP  ALL TIMER AND DECLARE A WINNER.
+            TimerBlue.Enabled = False
+            TimerRed.Enabled = False
+            TimerGreen.Enabled = False
+            Button2.Enabled = True
+            MsgBox("Green is Winner")
+
+        End If
+
+
+    End Sub
+
+End Class
